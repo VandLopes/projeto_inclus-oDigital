318c6b0cd5466c8ac9fbdc6c99d5f091b8b122f4
VANDERLEI LÓPES DE MEDEIROS <vanderlei_medeiros@estudante.sesisenai.org.br>
2026-02-10 22:02:16 -0300

    iniciando projeto de inclusão digital

---
+++ script.js
@@ -0,0 +1,24 @@
+const loginCorreto = "vanderleilopes23.vl@gmail.com";
+const senhaCorreta = "123456";
+
+const form = document.querySelector(".login-box");
+const loginInput = document.querySelector("input[type='email']");
+const passwordInput = document.querySelector("input[type='password']");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    const emailDigitado = loginInput.value;
+    const senhaDigitado = passwordInput.value;
+
+    if (emailDigitado == loginCorreto & senhaDigitado == senhaCorreta){
+
+        alert("Login efetuado com sucesso!")
+        window.location.href = 'home.html'
+    } else {
+
+        alert("Email ou senha incorretos!")
+    }
+    
+});
+
